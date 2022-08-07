44285c30d3c38c119eb29be5538852e778a104e6
Student <league@iMac-37.attlocal.net>
2022-08-07 14:30:40 -0700

    Done 2:30 PM Aug 7 2022

--- src/_02_nested_loops/_2_amazing_rings/AmazingRings.java
+++ src/_02_nested_loops/_2_amazing_rings/AmazingRings.java
@@ -1,6 +1,6 @@
 package _02_nested_loops._2_amazing_rings;
 
-import processing.core.PApplet;
+import processing.core.PApplet; 
 import processing.core.PImage;
 
 /*
@@ -10,33 +10,37 @@ import processing.core.PImage;
  *    file in this folder.
  * 
  * In the setup() method:
- * 2. Set the size of your window to be a rectangle like in the recipe
- *    demonstration
- * 3. Call the noFill() command so all the ellipses will be transparent
- *
+  2. Set the size of your window to be a rectangle like in the recipe
+     demonstration*/
+ 
+ /*3. Call the noFill() command so all the ellipses will be transparent*/
+  
+ /*
  * In the draw() method:
- * 4. Use a for loop to make the first set of rings that will start in the
- *    left half of the window (you can use your code from Bullseye).
- * 
- * 5. Make this set of rings move across the sketch to the right
+  4. Use a for loop to make the first set of rings that will start in the*/
+  
+ /* 5. Make this set of rings move across the sketch to the right
  *    Hint: make two variables, one for x and another for the speed.
- *    Then increase x by the amount in speed.
- *
- * 6. When the rings reach the right side of the sketch, reverse the direction
- *    so they move
- *    Hint: speed = -speed
+ *    Then increase x by the amount in speed.*/
+ 
+ /* 6. When the rings reach the right side of the sketch, reverse the direction
+     so they move
+     Hint: speed = -speed*/
       
- * 7. When the rings reach the left side of the sketch, reverse the direction
- *    again.
- * 
- * CHALLENGE - to finish the Amazing Rings
+ /* 7. When the rings reach the left side of the sketch, reverse the direction
+     again.*/
+  
+ /* CHALLENGE - to finish the Amazing Rings
  * 8. Add another for loop to draw the second set of rings that will start in
- *    the right half of the window
- *
- * 9. Make this set of rings move in the opposite direction to the other rings
- *    These rings must also "bounce" off the sides of the window.
- */
+     the right half of the window*/
+ 
+ /*9. Make this set of rings move in the opposite direction to the other rings
+ *    These rings must also "bounce" nt xOne = 250;
+*/
 public class AmazingRings extends PApplet {
+	int xSpeed = 1;
+	int xOne = 250;
+	int xTwo = 550;
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
 
@@ -44,7 +48,7 @@ public class AmazingRings extends PApplet {
 
     @Override
     public void settings() {
-
+    size(800,500);
     }
 
     @Override
@@ -54,7 +58,14 @@ public class AmazingRings extends PApplet {
 
     @Override
     public void draw() {
-
+    	noFill();
+    	background(255,255,255);
+    	for(int i=0; i<100; i++) {
+    		circle(xOne,250, i*7);
+    		circle(xTwo,250, i*7);
+    	}
+    	xOne+=xSpeed;
+    	xTwo-=xSpeed;
     }
 
     static public void main(String[] args) {
