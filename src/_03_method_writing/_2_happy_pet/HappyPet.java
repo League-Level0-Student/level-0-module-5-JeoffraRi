7e5619ef6bcbd603f6b5587ae77c2b7f9c328014
Student <league@iMac-20.attlocal.net>
2022-09-04 14:27:34 -0700

    All done! Sep 4 2022 2:27 PM

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -9,7 +9,24 @@ public class HappyPet {
 	
 	public static void main(String[] args) {
 		// 2. Ask the user what kind of pet they want to buy, and store their answer in a variable
-			JOptionPane.showMessageDialog(null, "Welcome to Keith's Klaws, the best cat store in San Diego!");
+		int start = JOptionPane.showOptionDialog(null, "Surfer Cat! 2022 edition", null, 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] {"Play", "Credits"}, null);
+		if (start==1) {
+			JOptionPane.showMessageDialog(null, "2022 RYALEX Studios");
+			JOptionPane.showMessageDialog(null, "Code writing\nAsher Law");
+			JOptionPane.showMessageDialog(null, "Assistance\nKeith Groves\nSamuel Sharp");
+			JOptionPane.showMessageDialog(null, "Guidelines\nLEAGUE of Amazing Programmers");
+			JOptionPane.showMessageDialog(null, "Software\nEclipse IDE");
+			JOptionPane.showMessageDialog(null, "Hardware\nApple Macintosh (desktop)");
+			JOptionPane.showMessageDialog(null, "Code managing\nGithub");
+			JOptionPane.showMessageDialog(null, "Office enviroment\nHacienda Del Mar");
+			JOptionPane.showMessageDialog(null, "Stimulation\nSprouts® cinnamon almonds");
+			int choice = JOptionPane.showOptionDialog(null, "Surfer Cat! 2022 edition", null, 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] {"Play", "Exit"}, null);
+				if (choice==1) {
+					System.exit(0);
+				}
+		}
 		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
 		
 			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
@@ -17,36 +34,61 @@ public class HappyPet {
 			//    Make sure to customize the title and question too.
 			for (int i=0; i<20; i++) {
 			int task = JOptionPane.showOptionDialog(null, "Do something with your cat, man.", "What do you wanna do with your cat?", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] {"Feed", "Drink", "Cuddle", "Groom", "Play with" }, null);
-			System.out.println(task);
+					new String[] {"Feed", "Drink", "Chill", "Cough", "Play with" }, null);
+			System.out.println(happinessLevel);
+			//System.out.println(task);
 			// 6. Use user input to call the appropriate method created in step 5 below.
 
 			// 7. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
+			if (task==0) {
+				feed();
+			}else if (task==1) {
+				drink();
+			}else if (task==2) {
+				chill();
+			}else if (task==3) {
+				cough();
+			}else if (task==4) {
+				playWith();
+			}else if (task==-1) {
+				JOptionPane.showMessageDialog(null, "Hey, are you turning off the game? Wait, no-");
+				System.exit(0);
 			}
-			JOptionPane.showMessageDialog(null, "Your cat is happy enough. Don't try to make him any happier,\n or he will get mad and punch you in the stomach!");
+			
 	}
-
+			JOptionPane.showMessageDialog(null, "Your cat is happy enough.");
+	}
+	
+	
+/*
+4: Feed
+3: Drink
+2: Cuddle
+1: Groom
+0: Play with
+-1: Close window
+	*/
 	// 5. Create methods to handle each of your user selections.
 	static void feed() {
-		JOptionPane.showMessageDialog(null, "You open a can of tuna. Your cat's eyes immediately \nsnap to the can, and he quickly devours it.");
+		JOptionPane.showMessageDialog(null, "You open a can of tuna. Your cat stands up and grabs a fork. \"Dude, did you think I was gonna eat so \n unsophisticated? Uncool, man.\" He then quickly devours it.");
 		happinessLevel+=1;
 		} 
 	static void drink() {
-		JOptionPane.showMessageDialog(null, "You fill your cat's water bowl. \nHe mews and happily laps it up.");
+		JOptionPane.showMessageDialog(null, "You fill your cat's water bowl. He says, \"Thanks for the water, dude,\" and happily laps it up.");
 		happinessLevel+=1;
 	}
-	static void cuddle() {
-		JOptionPane.showMessageDialog(null, "Your cat walks up to you, rubbing his \nface affectionately on you. He purrs.");
+	static void chill() {
+		JOptionPane.showMessageDialog(null, "Your cat walks up to you. \"Hey dude. How's it hangin'?\" \nYou pet your apparently anthropomorphic cat.");
 		happinessLevel+=1;
 	}
-	static void groom() {
-		JOptionPane.showMessageDialog(null, "You brush your cat's fur. The brush looks like it has been coated with thick cotton.");
+	static void cough() {
+		JOptionPane.showMessageDialog(null, "You pat your cat's back and a hairball shoots out of his mouth. \n \"Thanks for gettin' that out, dude. Totally cool, bro.\"");
 		happinessLevel+=1;
 	}
 	
 	static void playWith() {
-		JOptionPane.showMessageDialog(null, "You take out your cat's favorite feather toy and wave it around.");
+		JOptionPane.showMessageDialog(null, "You take out your cat's favorite feather toy and wave it around. \n \"Nah, man. I'm gonna play some DragonDungeon.io instead, dude.\"");
 		happinessLevel+=1;
 	}
 	
--- src/_03_method_writing/_4_calculator/Calculator.java
+++ src/_03_method_writing/_4_calculator/Calculator.java
@@ -12,15 +12,40 @@ public class Calculator {
 		int number2 = Integer.parseInt(input2);
 		int task = JOptionPane.showOptionDialog(null, "What do you choose?", "Calculator", 0,
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "divide", "multiply", "subtract", "add" }, null);
+		System.out.println(task);
 
+		/*3: add
+		 * 2: subtract
+		 * 1:multiply
+		 0: divide*/
 		// 3) Call the correct method depending on what option the user chooses
-		
+		if (task==3) {
+			JOptionPane.showMessageDialog(null, add(number1,number2));
+		}else if (task==2) {
+			JOptionPane.showMessageDialog(null, sub(number1,number2));
+		}else if (task==1) {
+			JOptionPane.showMessageDialog(null, mult(number1,number2));
+		}else if (task==0) {
+			JOptionPane.showMessageDialog(null, div(number1,number2));
+		}
 		// 4) Call the result() method and put the answer in a pop-up
 	}
 	
 	// 1) Make 4 static methods (add, subtract, multiply, divide)
 	//		They should each take 2 numbers as parameters
 	//		and return the answer.
+	static int add(int input1,int input2) {
+		return input1+input2;
+	} 
+	static int sub(int input1,int input2) {
+		return input1-input2;
+	}
+	static int mult(int input1,int input2) {
+		return input1*input2;
+	}
+	static int div(int input1,int input2) {
+		return input1/input2;
+	}
 	 
 	
 	// 2) Make a static method called " result" that takes a number
